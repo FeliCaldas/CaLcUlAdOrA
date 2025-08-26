11660e82d626701120d69f528a7ac88095ff689f
FeliCaldas <150372987+FeliCaldas@users.noreply.github.com>
2025-08-26 14:23:59 +0000

    sds

---
+++ script.js
@@ -0,0 +1,17 @@
+const display = document.getElementById("display");
+
+function adicionar(valor) {
+  display.value += valor;
+}
+
+function calcular() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Erro";
+  }
+}
+
+function limpar() {
+  display.value = "";
+}
